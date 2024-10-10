ef32296fbf365f5256ee5c4cf65401f3e73d570b
ezzat balah <ezzat.ahamed@icloud.com>
2024-10-11 02:26:54 +0300

    initlazize project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Islami());
+}
+
+class Islami extends StatelessWidget {
+  const Islami({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
